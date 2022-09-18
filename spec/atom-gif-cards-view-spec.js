f9e5188b472bbafe2ba93e175d9ee99dc126d08c
gamekub24 <113721416+gamekub24@users.noreply.github.com>
2022-09-19 03:08:33 +0700

    Delete atom-gif-cards.json

--- spec/atom-gif-cards-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomGifCardsView from '../lib/atom-gif-cards-view';
-
-describe('AtomGifCardsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
